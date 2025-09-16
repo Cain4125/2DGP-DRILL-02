076eb8db9f2b36491796595e97485da4f6b34844
park suhyeok <apato4125@gmail.com>
2025-09-16 15:57:52 +0900

    수업진행

--- character_move.py
+++ character_move.py
@@ -5,10 +5,13 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-
-grass.draw_now(400,30)
-character.draw_now(400,90)
-
-delay(5)
+x=0
+while(x<800):
+    clear_canvas()
+    grass.draw(400,30)
+    character.draw(x,90)
+    update_canvas()
+    x = x + 2
+    delay(0.01)
 
 close_canvas()
